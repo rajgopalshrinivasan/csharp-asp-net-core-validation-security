82f6758c1ae1d2ca1e6c791fa533d3c8ccddc671
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2020-01-14 16:08:35 -0500

    check work

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,18 +4,48 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker: IValidatableObject
     {
+        [Required]
         public int Id { get; set; }
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength:100, MinimumLength = 2)]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(maximumLength: 100, MinimumLength = 2)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
+        
+        [Required]
+        [StringLength(maximumLength: 500, MinimumLength = 10)]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            if(EmailAddress != null && EmailAddress.EndsWith("TechnologyLiveConference.com", StringComparison.OrdinalIgnoreCase))
+            {
+                validationResults.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+            }
+
+            return validationResults;
+
+        }
     }
 }
